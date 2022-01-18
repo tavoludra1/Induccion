6ebcb29519d9b9092a89192288fa309f71e68e20
GustavoP <tavoludra@icloud.com>
2022-01-18 11:41:37 -0500

    Initial Commit

---
+++ Induccion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Induccion
+//
+//  Created by Gustavo  Patiño on 18/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
