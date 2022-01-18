7cf9bb6832d8d6668063899a0e03fb10617c03fe
GustavoP <tavoludra@icloud.com>
2022-01-18 13:08:44 -0500

    01 Inicio proyecto con fondo azul

---
+++ Induccion/Controller/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  Induccion
+//
+//  Created by Gustavo  Patiño on 18/01/22.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+}
+
+
